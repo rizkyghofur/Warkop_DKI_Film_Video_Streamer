f7f1530a00d2b00181861baeb8070fafd0d619e2
RizkyGhofur <rizky.abdulghofur@gmail.com>
2020-10-30 08:06:16 +0700

    Fix pada MainActivity dimana tidak dapat mengirim data ke intent, penambahan Admob, dan perbaikan manifest

--- app/src/main/java/com/rizkyghofur/warkopdkifilmvideostreamer/MainActivity.java
+++ app/src/main/java/com/rizkyghofur/warkopdkifilmvideostreamer/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,11 +17,22 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
     List<Data> mListFilm;
     Data mData;
+    private AdView mAdView;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         mRecyclerView = findViewById(R.id.recyclerview);
         GridLayoutManager mGridLayoutManager = new GridLayoutManager(MainActivity.this, 2);
@@ -24,17 +40,18 @@ public class MainActivity extends AppCompatActivity {
 
         mListFilm = new ArrayList<>();
         mData = new Data("Mana Tahaaan... (1979)", getString(R.string.mana_tahan),
-                R.drawable.mana_tahan, "lontong");
+                R.drawable.mana_tahan, "STNo5NsfA40");
+        mListFilm.add(mData);
         mData = new Data("Gengsi Dong (1980)", getString(R.string.gengsi_dong),
-                R.drawable.gengsi_dong, "lontong");
+                R.drawable.gengsi_dong, "STNo5NsfA40");
         mListFilm.add(mData);
         mData = new Data("GeEr - Gede Rasa (1980)", getString(R.string.geer),
                 R.drawable.geeeer, "lontong");
         mListFilm.add(mData);
         mData = new Data("Pintar-Pintar Bodoh (1980)", getString(R.string.ppb),
-                R.drawable.pintar_pintar_bodoh, "lontong");
+                R.drawable.pintar_pintar_bodoh, "r6JS7FQ6fsE");
         mListFilm.add(mData);
-        mData = new Data("Manusia 6.000.000 Dolar (1981)", getString(R.string.manusia6jutadolar),
+        mData = new Data("Manusia 6 Juta Dolar (1981)", getString(R.string.manusia6jutadolar),
                 R.drawable.manusia_6_juta_dolar, "lontong");
         mListFilm.add(mData);
         mData = new Data("IQ Jongkok (1981)", getString(R.string.iqjongkok),
